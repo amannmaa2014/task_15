48cad374693be1a094eedfe6e4fd667bdfb54867
amannmaa2014 <amannmaa2014@gmail.com>
2019-02-19 19:17:19 +0300

    Nour_14

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,28 @@ class RestaurantListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = [
-        	'name',
-        	'opening_time',
-        	'closing_time',
-        	]
+            'name',
+            'opening_time',
+            'closing_time',
+            ]
+
+class RestaurantDetailSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    class Meta:
+        model = Restaurant
+        fields = [
+            'name',
+            'opening_time',
+            'closing_time',
+            'owner',
+            'id',
+            ]
+class RestaurantUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+            'name',
+            'opening_time',
+            'closing_time',
+            'description',
+            ]
